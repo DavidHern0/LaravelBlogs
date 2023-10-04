1850f67e44992e42d4911f171abc7d3a4b718ae4
DavidHern0 <dhernandezla0@gmail.com>
2023-10-04 17:03:51 +0200

    Navbar dropdown

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -12,27 +12,30 @@
     <header class="navbar navbar-expand-lg navbar-dark">
         <h1 class="mx-auto"><a class="navbar-brand text-decoration-none" href="/">{{ __('LaravelBlogs') }}</a></h1>
         <ul class="navbar-nav mx-auto">
-        @if (auth()->check())
-            <li class="nav-item">
-                <a class="nav-link btn btn-link text-decoration-none" href="{{ route('blog.create') }}">{{ __('Create new blog') }}</a>
-            </li>
-            <li class="nav-item">
-                <a class="nav-link btn btn-link text-decoration-none" href="{{ route('home.index') }}">{{__('My blogs')}}</a>
-            </li>
-            <li class="nav-item">    
-                <form action="{{ route('logout') }}" method="POST">
-                    @csrf
-                    <button class="btn btn-link nav-link text-decoration-none" type="submit">{{__('Logout')}}</button>
-                </form>
-            </li>
-        @else
-            <li class="nav-item">
-                <a class="nav-link btn btn-link text-decoration-none" href="{{ route('login.index') }}">{{__('Login')}}</a>
-            </li>
-            <li class="nav-item">
-                <a class="nav-link btn btn-link text-decoration-none" href="{{ route('register.index') }}">{{__('Register')}}</a>
-            </li>
-        @endif
+            @if (auth()->check())
+                <li class="nav-item">
+                    <a class="nav-link btn btn-link text-decoration-none" href="{{ route('blog.create') }}">{{ __('Create new blog') }}</a>
+                </li>
+                <li class="nav-item dropdown">
+                    <a class="nav-link btn btn-link text-decoration-none dropdown-toggle" href="#" id="navbarDropdown" role="button" data-bs-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                        {{ auth()->user()->name }}
+                    </a>
+                    <div class="dropdown-menu" aria-labelledby="navbarDropdown">
+                        <a class="dropdown-item" href="{{ route('home.index') }}">{{__('My blogs')}}</a>
+                        <form action="{{ route('logout') }}" method="POST">
+                            @csrf
+                            <button class="dropdown-item" type="submit">{{__('Logout')}}</button>
+                        </form>
+                    </div>
+                </li>
+            @else
+                <li class="nav-item">
+                    <a class="nav-link btn btn-link text-decoration-none" href="{{ route('login.index') }}">{{__('Login')}}</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link btn btn-link text-decoration-none" href="{{ route('register.index') }}">{{__('Register')}}</a>
+                </li>
+            @endif
         </ul>
     </header>
     
@@ -43,6 +46,7 @@
     <footer class="text-center mt-4">
         <p class="text-white">David Hernández Larrea &copy; {{ date('Y') }}</p>
     </footer>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js"></script>
     <script src="{{ asset('js/app.js') }}"></script>
 </body>
 </html>
