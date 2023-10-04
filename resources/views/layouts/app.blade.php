a093bab20e1cab09e15267cdc32ae9ac1623126c
DavidHern0 <dhernandezla0@gmail.com>
2023-10-04 18:27:19 +0200

    Style hotfix

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -6,21 +6,20 @@
     <meta name="csrf-token" content="{{ csrf_token() }}">
     <title>@yield('title')</title>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet">
+    <link rel="stylesheet" href="//fonts.googleapis.com/css?family=Open+Sans:300,400,600,700&amp;lang=en" />
     <link rel="stylesheet" href="{{ asset('css/app.css') }}">
 </head>
 <body>
-    <header class="navbar navbar-expand-lg navbar-dark">
-        <h1 class="mx-auto"><a class="navbar-brand text-decoration-none" href="/">{{ __('LaravelBlogs') }}</a></h1>
-        <ul class="navbar-nav mx-auto">
+    <header class="navbar navbar-expand navbar-dark d-flex justify-content-around align-items-baseline">
+        <h1 class=""><a class="navbar-brand text-decoration-none" href="/">{{ __('LaravelBlogs') }}</a></h1>
+        <ul class="navbar-nav">
             @if (auth()->check())
-                <li class="nav-item">
-                    <a class="nav-link btn btn-link text-decoration-none" href="{{ route('blog.create') }}">{{ __('Create new blog') }}</a>
-                </li>
                 <li class="nav-item dropdown">
                     <a class="nav-link btn btn-link text-decoration-none dropdown-toggle" href="#" id="navbarDropdown" role="button" data-bs-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
                         {{ auth()->user()->name }}
                     </a>
                     <div class="dropdown-menu" aria-labelledby="navbarDropdown">
+                        <a class="dropdown-item" href="{{ route('blog.create') }}">{{ __('Create new blog') }}</a>
                         <a class="dropdown-item" href="{{ route('home.index') }}">{{__('My blogs')}}</a>
                         <form action="{{ route('logout') }}" method="POST">
                             @csrf
