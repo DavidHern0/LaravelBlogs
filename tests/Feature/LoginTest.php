d6dd413cba76b444479101a6b31e898370394740
DavidHern0 <dhernandezla0@gmail.com>
2023-10-04 11:11:21 +0200

    Add LoginTest

---
+++ tests/Feature/LoginTest.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace Tests\Feature;
+
+use Illuminate\Foundation\Testing\RefreshDatabase;
+use Illuminate\Foundation\Testing\WithFaker;
+use Illuminate\Support\Facades\Hash;
+use Tests\TestCase;
+use App\Models\User;
+
+class LoginTest extends TestCase
+{
+    use RefreshDatabase;
+
+    public function test_user_can_login()
+    {
+        $user = User::factory()->create([
+            'email' => 'test@example.com',
+            'password' => Hash::make('password123'),
+        ]);
+        $response = $this->post(route('login'), [
+            'email' => 'test@example.com',
+            'password' => 'password123',
+        ]);
+
+        $this->assertAuthenticatedAs($user);
+    }
+}
